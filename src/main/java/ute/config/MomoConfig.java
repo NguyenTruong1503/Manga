6c6cd6e4b94530d189d43f88654180bfbaf90c91
NguyenTruong1503 <140087137+NguyenTruong1503@users.noreply.github.com>
2025-03-21 21:21:52 +0700

    fix_lan_2

--- src/main/java/ute/config/MomoConfig.java
+++ src/main/java/ute/config/MomoConfig.java
@@ -9,7 +9,7 @@ public class MomoConfig {
     public static final String ACCESS_KEY = "F8BBA842ECF85";
     public static final String SECRET_KEY = "K951B6PE1waDMi640xX08PD3vg6EkVlz";
     public static final String REQUEST_TYPE = "payWithMethod";
-    public static final String REDIRECT_URL = "http://localhost:5173/payment_return";
-    public static final String IPN_URL = "http://localhost:8080/orders/momo_return";
+    public static final String REDIRECT_URL = "https://web-sach-tre0-8-fe.vercel.app/payment_return";
+    public static final String IPN_URL = "https://manga-m8n9.onrender.com/orders/momo_return";
     public static final String ENDPOINT = "https://test-payment.momo.vn/v2/gateway/api/create";
 }
